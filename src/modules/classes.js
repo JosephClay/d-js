aaf702552b3d1448fcee6718cf88e1cfa40cc6c5
Andrew C. Dvorak <acdvorak@gmail.com>
2014-04-23 17:23:52 -0500

    Progress on classes.js

--- src/modules/classes.js
+++ src/modules/classes.js
@@ -1,40 +1,149 @@
-defined([ 'overload' ], function(overload) {
+define([ '_', 'overload', 'utils' ], function(_, overload, utils) {
 
-    return {
-        addClass: function(name) {
-            if (this.elem.classList) {
-                this.elem.classList.add(name);
-                return this;
-            }
+    var _rspace = /\s+/g;
 
-            this.elem.className += ' ' + name;
-            return this;
+    var _classArrayCache = {};
+    var _classMapCache = {};
+
+    var _isNotEmpty = function(str) { return str !== null && str !== undefined && str !== ''; };
+
+    // TODO: Create an internal cache
+    var _split = function(name) {
+        if (_.isArray(name)) { return name; }
+        return _classArrayCache[name] || (_classArrayCache[name] = _.chain(name.split(_rspace)).filter(_isNotEmpty).uniq().value());
+    };
+
+    var _modern = {
+        hasClass: function(elem, name) {
+            return elem.classList.contains(name);
         },
 
-        removeClass: function(name) {
-            if (this.elem.classList) {
-                this.elem.classList.remove(name);
-                return this;
-            }
-
-            this.elem.className = this.elem.className.replace(new RegExp('(^|\\b)' + name.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
-            return this;
+        addClass: function(elem, names) {
+            elem.classList.add.apply(null, names);
         },
 
-        hasClass: function() {},
-
-        toggleClass: function() {},
-
-        fn: {
-            addClass: overload().args(String).use(function(value) {
-                        classes.addClass(this._elems, [value]);
-                        return this;
-                    })
-                    .args(Array).use(function(values) {
-                        classes.addClass(this._elems, values);
-                        return this;
-                    })
-                    .expose()
+        removeClass: function(elem, names) {
+            elem.classList.remove.apply(null, names);
         }
     };
+
+    var _legacy = {
+        hasClass: function(elem, name) {
+            var elemClassNames = _classArrayCache[elem.className] || (_classArrayCache[elem.className] = _split(elem.className)),
+                idx = elemClassNames.length;
+            while (idx--) {
+                if (elemClassNames[idx] === name) { return true; }
+            }
+            return false;
+        },
+
+        addClass: function(elem, names) {
+            var elemClassNameArray = _classArrayCache[elem.className] || (_classArrayCache[elem.className] = _split(elem.className)),
+                elemClassNameMap = _classMapCache[elem.className] || (_classMapCache[elem.className] = _.object(elemClassNameArray)),
+                nameIdx = elemClassNameArray.length,
+                name,
+                append = '';
+
+            while (nameIdx--) {
+                name = names[nameIdx];
+
+                // Element already has this class name
+                if (elemClassNameMap[name] !== undefined) { continue; }
+
+                append += ' ' + name;
+            }
+
+            // Add all the class names in a single step
+            elem.className += append;
+        },
+
+        removeClass: function(elem, names) {
+            var elemClassNameArray = _classArrayCache[elem.className] || (_classArrayCache[elem.className] = _split(elem.className)),
+                elemClassNameMap = _classMapCache[elem.className] || (_classMapCache[elem.className] = _.object(elemClassNameArray)),
+                nameIdx = elemClassNameArray.length,
+                name,
+                newClasses = utils.slice(elemClassNameArray);
+
+            while (nameIdx--) {
+                name = names[nameIdx];
+
+                // Element has this class name
+                if (elemClassNameMap[name] !== undefined) {
+                    newClasses.splice(nameIdx, 1);
+                    elem.className = newClasses.join(' ');
+                    return;
+                }
+            }
+        }
+    };
+
+    // TODO: Make a global dummy div
+    var _impl = document.createElement('div').classList ? _modern : _legacy;
+
+    var _classes = {
+        hasClass: function(elems, names) {
+            var numElems = elems.length,
+                numNames = names.length,
+                elemIdx = numElems,
+                nameIdx,
+                elem,
+                name;
+
+            while (elemIdx--) {
+                elem = elems[elemIdx];
+
+                nameIdx = numNames;
+                while (nameIdx--) {
+                    name = names[nameIdx];
+
+                    if (_impl.hasClass(elem, name)) { return true; }
+                }
+            }
+
+            return false;
+        },
+
+        addClass: function(elems, names) {
+            var elemIdx = elems.length;
+            while (elemIdx--) {
+                _impl.addClass(elems[elemIdx], names);
+            }
+        },
+
+        removeClass: function(elems, names) {
+            var elemIdx = elems.length;
+            while (elemIdx--) {
+                _impl.removeClass(elems[elemIdx], names);
+            }
+        },
+
+        toggleClass: function() {}
+    };
+
+    return _.extend({}, _classes, {
+        fn: {
+            addClass: overload()
+                .args(String).use(function(name) {
+                    // TODO: Generalize this check?
+                    if (!this.length) { return this; }
+
+                    var names = _split(name);
+                    if (!names.length) { return this; }
+
+                    _classes.addClass(this._elems, names);
+
+                    return this;
+                })
+                .args(Array).use(function(names) {
+                    // TODO: Generalize this check?
+                    if (!this.length) { return this; }
+                    if (!names.length) { return this; }
+
+                    _classes.addClass(this._elems, names);
+
+                    return this;
+                })
+                .expose()
+        }
+    });
 });
