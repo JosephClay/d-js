298ce65bf17a146c02d38aa5f10ae49476039226
Joseph Clay <clay@dcim.com>
2014-04-28 17:20:24 -0500

    Continue underscore replacement development (WIP)

--- src/_.js
+++ src/_.js
@@ -1,8 +1,43 @@
-var _isNodeList = function(item) {
-    return item instanceof NodeList;
+var _ = {};
+
+_.exists = function(obj) {
+    return obj !== null && obj !== undefined;
 };
 
-var _flatten = function(arr) {
+_.isElement = function(obj) {
+    return !!(obj && obj.nodeType === 1);
+};
+
+_.isArray = Array.isArray || function(obj) {
+    return toString.call(obj) == '[object Array]';
+};
+
+// Add some isType methods: isArguments, isFunction, isString, isNumber, isDate, isRegExp.
+var types = ['Arguments', 'Function', 'String', 'Number', 'Date', 'RegExp'],
+    idx = types.length,
+    generateCheck = function(name) {
+        return function(obj) {
+            return toString.call(obj) == '[object ' + name + ']';
+        };
+    },
+    name;
+while (idx--) {
+    var name = types[idx];
+    _['is' + name] = generateCheck(name);
+}
+
+// Optimize `isFunction` if appropriate.
+if (typeof (/./) !== 'function') {
+    _.isFunction = function(obj) {
+        return typeof obj === 'function';
+    };
+}
+
+_.isNodeList = function(obj) {
+    return obj instanceof NodeList;
+};
+
+_.flatten = function(arr) {
     var result = [];
 
     var idx = 0, length = arr.length,
@@ -19,7 +54,61 @@ var _flatten = function(arr) {
 
     return result;
 };
-module.exports = {
-    flatten: _flatten,
-    isNodeList: _isNodeList
+
+_.every = function(arr, iterator) {
+    if (!_.exists(arr)) { return true; }
+
+    var idx = 0, length = arr.length;
+    for (; idx < length; idx++) {
+        if (!iterator(value, idx)) { return false; }
+    }
+
+    return true;
 };
+
+_.extend = function() {
+    var args = arguments,
+        obj = args[0],
+        idx = 1, length = args.length;
+
+    if (!obj) { return obj; }
+
+    for (; idx < length; idx++) {
+        var source = args[idx];
+        if (source) {
+            for (var prop in source) {
+                obj[prop] = source[prop];
+            }
+        }
+    }
+
+    return obj;
+};
+
+_.map = function(arr, iterator) {
+    var results = [];
+    if (!arr) { return results; }
+
+    var idx = 0, length = arr.length;
+    for (; idx < length; idx++) {
+        results.push(iterator(arr[idx], idx));
+    }
+
+    return results;
+};
+
+_.filter = function(arr, iterator) {
+    var results = [];
+    if (!arr) { return results; }
+
+    var idx = 0, length = arr.length;
+    for (; idx < length; idx++) {
+        if (iterator(arr[idx], idx)) {
+            results.push(value);
+        }
+    }
+
+    return results;
+};
+
+module.exports = _;
