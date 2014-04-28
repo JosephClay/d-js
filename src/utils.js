99fa16c8c704e7fcd859298590eea5529f3cb2f5
Joseph Clay <clay@dcim.com>
2014-04-28 16:30:43 -0500

    More robust isHTML test

--- src/utils.js
+++ src/utils.js
@@ -1,12 +1,20 @@
 var array = require('./modules/array');
 
+var _BEGINNING_NEW_LINES = /^[\n]*/;
+
 module.exports = {
     exists: function(val) {
         return (val !== null && val !== undefined);
     },
 
     isHTML: function(text) {
-        return _.isString(text) && (text.charAt(0) === '<' && text.charAt(text.length - 1) === '>' && text.length >= 3);
+        if (!_.isString(text)) { return false; }
+
+        // TODO: Using es5 native method (trim)
+        text = text.trim();
+        text = text.replace(_BEGINNING_NEW_LINES, '');
+
+        return (text.charAt(0) === '<' && text.charAt(text.length - 1) === '>' && text.length >= 3);
     },
 
     unique: function(results) {
