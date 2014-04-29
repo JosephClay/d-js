a35e7b3f6aaa17c4c1ac1ec45ddd6338e8a0a143
Joseph Clay <testingriven@gmail.com>
2014-04-29 09:18:56 -0500

    Development

--- tests/core.js
+++ tests/core.js
@@ -157,115 +157,114 @@ test('map()', function() {
 	);
 });
 
-test("D.extend(Object, Object)", function() {
+test('D.extend(Object, Object)', function() {
 	expect(28);
 
 	var empty, optionsWithLength, optionsWithDate, myKlass,
 		customObject, optionsWithCustomObject, MyNumber, ret,
 		nullUndef, target, recursive, obj,
 		defaults, defaultsCopy, options1, options1Copy, options2, options2Copy, merged2,
-		settings = { "xnumber1": 5, "xnumber2": 7, "xstring1": "peter", "xstring2": "pan" },
-		options = { "xnumber2": 1, "xstring2": "x", "xxx": "newstring" },
-		optionsCopy = { "xnumber2": 1, "xstring2": "x", "xxx": "newstring" },
-		merged = { "xnumber1": 5, "xnumber2": 1, "xstring1": "peter", "xstring2": "x", "xxx": "newstring" },
-		deep1 = { "foo": { "bar": true } },
-		deep2 = { "foo": { "baz": true }, "foo2": document },
-		deep2copy = { "foo": { "baz": true }, "foo2": document },
-		deepmerged = { "foo": { "bar": true, "baz": true }, "foo2": document },
+		settings = { 'xnumber1': 5, 'xnumber2': 7, 'xstring1': 'peter', 'xstring2': 'pan' },
+		options = { 'xnumber2': 1, 'xstring2': 'x', 'xxx': 'newstring' },
+		optionsCopy = { 'xnumber2': 1, 'xstring2': 'x', 'xxx': 'newstring' },
+		merged = { 'xnumber1': 5, 'xnumber2': 1, 'xstring1': 'peter', 'xstring2': 'x', 'xxx': 'newstring' },
+		deep1 = { 'foo': { 'bar': true } },
+		deep2 = { 'foo': { 'baz': true }, 'foo2': document },
+		deep2copy = { 'foo': { 'baz': true }, 'foo2': document },
+		deepmerged = { 'foo': { 'bar': true, 'baz': true }, 'foo2': document },
 		arr = [1, 2, 3],
-		nestedarray = { "arr": arr };
+		nestedarray = { 'arr': arr };
 
 	D.extend(settings, options);
-	deepEqual( settings, merged, "Check if extended: settings must be extended" );
-	deepEqual( options, optionsCopy, "Check if not modified: options must not be modified" );
+	deepEqual(settings, merged, 'Check if extended: settings must be extended' );
+	deepEqual(options, optionsCopy, 'Check if not modified: options must not be modified' );
 
 	D.extend(settings, null, options);
-	deepEqual( settings, merged, "Check if extended: settings must be extended" );
-	deepEqual( options, optionsCopy, "Check if not modified: options must not be modified" );
+	deepEqual( settings, merged, 'Check if extended: settings must be extended' );
+	deepEqual( options, optionsCopy, 'Check if not modified: options must not be modified' );
 
 	D.extend(true, deep1, deep2);
-	deepEqual( deep1["foo"], deepmerged["foo"], "Check if foo: settings must be extended" );
-	deepEqual( deep2["foo"], deep2copy["foo"], "Check if not deep2: options must not be modified" );
-	equal( deep1["foo2"], document, "Make sure that a deep clone was not attempted on the document" );
+	deepEqual(deep1.foo, deepmerged.foo, 'Check if foo: settings must be extended');
+	deepEqual(deep2.foo, deep2copy.foo, 'Check if not deep2: options must not be modified');
+	equal(deep1.foo2, document, 'Make sure that a deep clone was not attempted on the document');
 
-	ok( D.extend(true, {}, nestedarray)["arr"] !== arr, "Deep extend of object must clone child array" );
+	ok(D.extend(true, {}, nestedarray).arr !== arr, 'Deep extend of object must clone child array');
 
 	// #5991
-	ok( D.isArray( D.extend(true, { "arr": {} }, nestedarray)["arr"] ), "Cloned array have to be an Array" );
-	ok( D.isPlainObject( D.extend(true, { "arr": arr }, { "arr": {} })["arr"] ), "Cloned object have to be an plain object" );
+	ok(_.isArray(D.extend(true, { 'arr': {} }, nestedarray).arr), 'Cloned array have to be an Array');
+	ok(_.isObject(D.extend(true, { 'arr': arr }, { 'arr': {} }).arr), 'Cloned object have to be an plain object');
 
 	empty = {};
-	optionsWithLength = { "foo": { "length": -1 } };
+	optionsWithLength = { 'foo': { 'length': -1 } };
 	D.extend(true, empty, optionsWithLength);
-	deepEqual( empty["foo"], optionsWithLength["foo"], "The length property must copy correctly" );
+	deepEqual(empty.foo, optionsWithLength.foo, 'The length property must copy correctly');
 
 	empty = {};
-	optionsWithDate = { "foo": { "date": new Date() } };
+	optionsWithDate = { 'foo': { 'date': new Date() }};
 	D.extend(true, empty, optionsWithDate);
-	deepEqual( empty["foo"], optionsWithDate["foo"], "Dates copy correctly" );
+	deepEqual(empty.foo, optionsWithDate.foo, 'Dates copy correctly');
 
 	/** @constructor */
 	myKlass = function() {};
 	customObject = new myKlass();
-	optionsWithCustomObject = { "foo": { "date": customObject } };
+	optionsWithCustomObject = { 'foo': { 'date': customObject } };
 	empty = {};
 	D.extend(true, empty, optionsWithCustomObject);
-	ok( empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly (no methods)" );
+	ok( empty.foo && empty.foo.date === customObject, 'Custom objects copy correctly (no methods)');
 
 	// Makes the class a little more realistic
-	myKlass.prototype = { "someMethod": function(){} };
+	myKlass.prototype = { 'someMethod': function(){} };
 	empty = {};
 	D.extend(true, empty, optionsWithCustomObject);
-	ok( empty["foo"] && empty["foo"]["date"] === customObject, "Custom objects copy correctly" );
+	ok(empty.foo && empty.foo.date === customObject, 'Custom objects copy correctly');
 
 	MyNumber = Number;
 
-	ret = D.extend(true, { "foo": 4 }, { "foo": new MyNumber(5) } );
-	ok( parseInt(ret.foo, 10) === 5, "Wrapped numbers copy correctly" );
+	ret = D.extend(true, { 'foo': 4 }, { 'foo': new MyNumber(5) });
+	ok( parseInt(ret.foo, 10) === 5, 'Wrapped numbers copy correctly');
 
-	nullUndef;
-	nullUndef = D.extend({}, options, { "xnumber2": null });
-	ok( nullUndef["xnumber2"] === null, "Check to make sure null values are copied");
+	nullUndef = D.extend({}, options, { 'xnumber2': null });
+	ok( nullUndef.xnumber2 === null, 'Check to make sure null values are copied');
 
-	nullUndef = D.extend({}, options, { "xnumber2": undefined });
-	ok( nullUndef["xnumber2"] === options["xnumber2"], "Check to make sure undefined values are not copied");
+	nullUndef = D.extend({}, options, { 'xnumber2': undefined });
+	ok( nullUndef.xnumber2 === options.xnumber2, 'Check to make sure undefined values are not copied');
 
-	nullUndef = D.extend({}, options, { "xnumber0": null });
-	ok( nullUndef["xnumber0"] === null, "Check to make sure null values are inserted");
+	nullUndef = D.extend({}, options, { 'xnumber0': null });
+	ok( nullUndef.xnumber0 === null, 'Check to make sure null values are inserted');
 
 	target = {};
 	recursive = { foo:target, bar:5 };
 	D.extend(true, target, recursive);
-	deepEqual( target, { bar:5 }, "Check to make sure a recursive obj doesn't go never-ending loop by not copying it over" );
+	deepEqual( target, { bar:5 }, 'Check to make sure a recursive obj doesnt go never-ending loop by not copying it over' );
 
 	ret = D.extend(true, { foo: [] }, { foo: [0] } ); // 1907
-	equal( ret.foo.length, 1, "Check to make sure a value with coercion 'false' copies over when necessary to fix #1907" );
+	equal( ret.foo.length, 1, 'Check to make sure a value with coercion "false" copies over when necessary to fix #1907' );
 
-	ret = D.extend(true, { foo: "1,2,3" }, { foo: [1, 2, 3] } );
-	ok( typeof ret.foo !== "string", "Check to make sure values equal with coercion (but not actually equal) overwrite correctly" );
+	ret = D.extend(true, { foo: '1,2,3' }, { foo: [1, 2, 3] } );
+	ok( typeof ret.foo !== 'string', 'Check to make sure values equal with coercion (but not actually equal) overwrite correctly' );
 
-	ret = D.extend(true, { foo:"bar" }, { foo:null } );
-	ok( typeof ret.foo !== "undefined", "Make sure a null value doesn't crash with deep extend, for #1908" );
+	ret = D.extend(true, { foo: 'bar' }, { foo: null } );
+	ok( typeof ret.foo !== 'undefined', 'Make sure a null value doesnt crash with deep extend, for #1908' );
 
-	obj = { foo:null };
-	D.extend(true, obj, { foo:"notnull" } );
-	equal( obj.foo, "notnull", "Make sure a null value can be overwritten" );
+	obj = { foo: null };
+	D.extend(true, obj, { foo: 'notnull' } );
+	equal( obj.foo, 'notnull', 'Make sure a null value can be overwritten' );
 
 	function func() {}
-	D.extend(func, { key: "value" } );
-	equal( func.key, "value", "Verify a function can be extended" );
+	D.extend(func, { key: 'value' } );
+	equal( func.key, 'value', 'Verify a function can be extended' );
 
-	defaults = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" };
-	defaultsCopy = { xnumber1: 5, xnumber2: 7, xstring1: "peter", xstring2: "pan" };
-	options1 = { xnumber2: 1, xstring2: "x" };
-	options1Copy = { xnumber2: 1, xstring2: "x" };
-	options2 = { xstring2: "xx", xxx: "newstringx" };
-	options2Copy = { xstring2: "xx", xxx: "newstringx" };
-	merged2 = { xnumber1: 5, xnumber2: 1, xstring1: "peter", xstring2: "xx", xxx: "newstringx" };
+	defaults = { xnumber1: 5, xnumber2: 7, xstring1: 'peter', xstring2: 'pan' };
+	defaultsCopy = { xnumber1: 5, xnumber2: 7, xstring1: 'peter', xstring2: 'pan' };
+	options1 = { xnumber2: 1, xstring2: 'x' };
+	options1Copy = { xnumber2: 1, xstring2: 'x' };
+	options2 = { xstring2: 'xx', xxx: 'newstringx' };
+	options2Copy = { xstring2: 'xx', xxx: 'newstringx' };
+	merged2 = { xnumber1: 5, xnumber2: 1, xstring1: 'peter', xstring2: 'xx', xxx: 'newstringx' };
 
 	settings = D.extend({}, defaults, options1, options2);
-	deepEqual( settings, merged2, "Check if extended: settings must be extended" );
-	deepEqual( defaults, defaultsCopy, "Check if not modified: options1 must not be modified" );
-	deepEqual( options1, options1Copy, "Check if not modified: options1 must not be modified" );
-	deepEqual( options2, options2Copy, "Check if not modified: options2 must not be modified" );
+	deepEqual( settings, merged2, 'Check if extended: settings must be extended' );
+	deepEqual( defaults, defaultsCopy, 'Check if not modified: options1 must not be modified' );
+	deepEqual( options1, options1Copy, 'Check if not modified: options1 must not be modified' );
+	deepEqual( options2, options2Copy, 'Check if not modified: options2 must not be modified' );
 });
