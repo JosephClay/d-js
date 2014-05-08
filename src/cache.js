1faf5af7f32431d451f8ddfe22529af9a5fff6e7
Joseph Clay <clay@dcim.com>
2014-05-08 13:36:11 -0500

    BiLevel cache

--- src/cache.js
+++ src/cache.js
@@ -3,7 +3,6 @@ var _ = require('_');
 var _cache = {},
 
     _getterSetter = function(key) {
-
         var ref = (_cache[key] = {});
 
         return {
@@ -20,8 +19,34 @@ var _cache = {},
                 return (ref[key] = fn());
             }
         };
+    },
+
+    _biLevelGetterSetter = function(key) {
+        var ref = (_cache[key] = {});
+
+        return {
+            get: function(key1, key2) {
+                var ref1 = ref[key];
+                return ref1 !== undefined ? ref1[key2] : ref1;
+            },
+            set: function(key1, key2, value) {
+                var ref1 = ref[key1] || (ref[key1] = {});
+                ref1[key2] = value;
+                return value;
+            },
+            getOrSet: function(key1, key2, fn) {
+                var ref1 = ref[key1],
+                    cachedVal = ref1[key2];
+                if (cachedVal !== undefined) { return cachedVal; }
+                return (ref1[key2] = fn());
+            }
+        };
     };
 
-module.exports = function(name) {
+var api = function(name) {
     return _getterSetter(name || _.uniqueId());
 };
+api.biLevel = function(name) {
+    return _biLevelGetterSetter(name || _.uniqueId());
+};
+module.exports = api;
