c0988342ca903c586f44958934addfac1471eb01
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-09 15:08:23 -0500

    Minor/ignore - Typo

--- src/modules/fizzle.js
+++ src/modules/fizzle.js
@@ -1,4 +1,4 @@
-// TODO: Move into "query" dir and rename "qeuery" to "fizzle". Make those scripts all one engine
+// TODO: Move into "query" dir and rename "query" to "fizzle". Make those scripts all one engine
 /*!
  * Sizzle CSS Selector Engine v1.10.20-pre
  * http://sizzlejs.com/
