97ed455cd666cd74385bb43115574163739f8e9d
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-09 14:39:54 -0500

    Avoid name collisions in cache.js

--- src/cache.js
+++ src/cache.js
@@ -1,17 +1,29 @@
 var _ = require('_');
 
+/**
+ * From Sizzle.js `createCache()`:
+ * Use (key + ' ') to avoid collision with native prototype properties.
+ * @param {String} key
+ * @returns {String}
+ * @private
+ */
+var _safe = function(key) { return key + ' '; };
+
 var _getterSetter = function() {
         var ref = {};
 
         return {
             get: function(key) {
+                key = _safe(key);
                 return ref[key];
             },
             set: function(key, value) {
+                key = _safe(key);
                 ref[key] = value;
                 return value;
             },
             getOrSet: function(key, fn) {
+                key = _safe(key);
                 var cachedVal = ref[key];
                 if (cachedVal !== undefined) { return cachedVal; }
                 return (ref[key] = fn());
@@ -24,15 +36,21 @@ var _getterSetter = function() {
 
         return {
             get: function(key1, key2) {
+                key1 = _safe(key1);
+                key2 = _safe(key2);
                 var ref1 = ref[key1];
                 return ref1 !== undefined ? ref1[key2] : ref1;
             },
             set: function(key1, key2, value) {
+                key1 = _safe(key1);
+                key2 = _safe(key2);
                 var ref1 = ref[key1] || (ref[key1] = {});
                 ref1[key2] = value;
                 return value;
             },
             getOrSet: function(key1, key2, fn) {
+                key1 = _safe(key1);
+                key2 = _safe(key2);
                 var ref1 = ref[key1] || (ref[key1] = {}),
                     cachedVal = ref1[key2];
                 if (cachedVal !== undefined) { return cachedVal; }
