258f64d2af1ca014cb5f3eb1ebebcb9eb3a46ff0
Joseph Clay <testingriven@gmail.com>
2014-05-12 15:47:36 -0500

    Test script updates

--- tests/showhide.js
+++ tests/showhide.js
@@ -1,78 +1,46 @@
 (function() {
 
-    module("show/hide");
+    module('show/hide');
 
-    test("sanity check", function() {
+    test('sanity check', function() {
         expect(1);
-        ok( jQuery("#dl:visible, #qunit-fixture:visible, #foo:visible").length === 3, "QUnit state is correct for testing effects" );
+        ok(D('#dl:visible, #qunit-fixture:visible, #foo:visible').length === 3, 'QUnit state is correct for testing effects');
     });
 
-    test("show() basic", 2, function() {
+    test('show() basic', function() {
+        expect(2);
+
         var div,
-            hiddendiv = jQuery("div.hidden");
+            hiddendiv = D('div.hidden');
 
         hiddendiv.hide().show();
 
-        equal( hiddendiv.css("display"), "block", "Make sure a pre-hidden div is visible." );
+        equal(hiddendiv.css('display'), 'block', 'Make sure a pre-hidden div is visible.');
 
-        div = jQuery("<div>").hide().appendTo("#qunit-fixture").show();
+        div = D('<div>').hide().appendTo('#qunit-fixture').show();
 
-        equal( div.css("display"), "block", "Make sure pre-hidden divs show" );
+        equal(div.css('display'), 'block', 'Make sure pre-hidden divs show');
 
         // Clean up the detached node
         div.remove();
-
-        QUnit.expectJqData(hiddendiv, "olddisplay");
     });
 
-    // Supports #7397
-    test("Persist correct display value", function() {
-        expect(3);
-
-        // #show-tests * is set display: none in CSS
-        jQuery("#qunit-fixture").append("<div id='show-tests'><span style='position:absolute;'>foo</span></div>");
-
-        var $span = jQuery("#show-tests span"),
-            displayNone = $span.css("display"),
-            display = "",
-            clock = this.clock;
-
-        $span.show();
-
-        display = $span.css("display");
-
-        $span.hide();
-
-        $span.fadeIn(100, function() {
-            equal($span.css("display"), display, "Expecting display: " + display);
-            $span.fadeOut(100, function () {
-                equal($span.css("display"), displayNone, "Expecting display: " + displayNone);
-                $span.fadeIn(100, function() {
-                    equal($span.css("display"), display, "Expecting display: " + display);
-                });
-            });
-        });
-
-        clock.tick( 300 );
-
-        QUnit.expectJqData($span, "olddisplay");
-    });
-
-    test("toggle()", function() {
+    test('toggle()', function() {
         expect(6);
-        var x = jQuery("#foo");
-        ok( x.is(":visible"), "is visible" );
+
+        var x = D('#foo');
+        ok(x.is(':visible'), 'is visible');
         x.toggle();
-        ok( x.is(":hidden"), "is hidden" );
+        ok(x.is(':hidden'), 'is hidden');
         x.toggle();
-        ok( x.is(":visible"), "is visible again" );
+        ok(x.is(':visible'), 'is visible again');
 
         x.toggle(true);
-        ok( x.is(":visible"), "is visible" );
+        ok(x.is(':visible'), 'is visible');
         x.toggle(false);
-        ok( x.is(":hidden"), "is hidden" );
+        ok(x.is(':hidden'), 'is hidden');
         x.toggle(true);
-        ok( x.is(":visible"), "is visible again" );
+        ok(x.is(':visible'), 'is visible again');
     });
 
 })();
