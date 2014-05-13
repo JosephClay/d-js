372b491e7f201eb3b54274b062b10e2b4f2e7ed9
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-13 14:43:11 -0500

    Encapsulate fizzle test code in self-invoking function

--- tests/fizzle.js
+++ tests/fizzle.js
@@ -1,154 +1,158 @@
-module('fizzle');
+(function() {
 
-var QUERIES = {
-    'Without spaces': {
-        'Without tag names': {
-            'Double quotes': [
-                [ 'a', '#id', '[some-attr]', '.class', '[some-attr=""]' ],
-                [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c"]' ],
-                [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\],d,e,f"]' ],
-                [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\",d,e,f"]' ],
-                [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\]\\",d,e,f"]' ],
-                [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\]\\",d,e,f"]' ],
-                [ 'a', '#id', '[some-attr]', '.class', '[some-attr="\\""]' ]
-            ],
+    module('fizzle');
 
-            'Single quotes': [
-                [ "a", "#id", "[some-attr]", ".class", "[some-attr='']" ],
-                [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c']" ],
-                [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\],d,e,f']" ],
-                [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\',d,e,f']" ],
-                [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\]\\',d,e,f']" ],
-                [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\]\\',d,e,f']" ],
-                [ "a", "#id", "[some-attr]", ".class", "[some-attr='\\'']" ]
-            ]
+    var QUERIES = {
+        'Without spaces': {
+            'Without tag names': {
+                'Double quotes': [
+                    [ 'a', '#id', '[some-attr]', '.class', '[some-attr=""]' ],
+                    [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c"]' ],
+                    [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\],d,e,f"]' ],
+                    [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\",d,e,f"]' ],
+                    [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\]\\",d,e,f"]' ],
+                    [ 'a', '#id', '[some-attr]', '.class', '[some-attr="a,b,c\\]\\",d,e,f"]' ],
+                    [ 'a', '#id', '[some-attr]', '.class', '[some-attr="\\""]' ]
+                ],
+
+                'Single quotes': [
+                    [ "a", "#id", "[some-attr]", ".class", "[some-attr='']" ],
+                    [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c']" ],
+                    [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\],d,e,f']" ],
+                    [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\',d,e,f']" ],
+                    [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\]\\',d,e,f']" ],
+                    [ "a", "#id", "[some-attr]", ".class", "[some-attr='a,b,c\\]\\',d,e,f']" ],
+                    [ "a", "#id", "[some-attr]", ".class", "[some-attr='\\'']" ]
+                ]
+            },
+
+            'With tag names': {
+                'Double quotes': [
+                    [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr=""]' ],
+                    [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c"]' ],
+                    [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\],d,e,f"]' ],
+                    [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\",d,e,f"]' ],
+                    [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\]\\",d,e,f"]' ],
+                    [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\]\\",d,e,f"]' ],
+                    [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="\\""]' ]
+                ],
+
+                'Single quotes': [
+                    [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='']" ],
+                    [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c']" ],
+                    [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\],d,e,f']" ],
+                    [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\',d,e,f']" ],
+                    [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\]\\',d,e,f']" ],
+                    [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\]\\',d,e,f']" ],
+                    [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='\\'']" ]
+                ]
+            }
         },
 
-        'With tag names': {
-            'Double quotes': [
-                [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr=""]' ],
-                [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c"]' ],
-                [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\],d,e,f"]' ],
-                [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\",d,e,f"]' ],
-                [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\]\\",d,e,f"]' ],
-                [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="a,b,c\\]\\",d,e,f"]' ],
-                [ 'a', '#id', 'input[some-attr]', '.class', 'input[some-attr="\\""]' ]
-            ],
+        'With spaces': {
+            'Without tag names': {
+                'Double quotes': [
+                    [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "" ]' ],
+                    [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c" ]' ],
+                    [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\],d,e,f" ]' ],
+                    [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\",d,e,f" ]' ],
+                    [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
+                    [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
+                    [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "\\"" ]' ]
+                ],
 
-            'Single quotes': [
-                [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='']" ],
-                [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c']" ],
-                [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\],d,e,f']" ],
-                [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\',d,e,f']" ],
-                [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\]\\',d,e,f']" ],
-                [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='a,b,c\\]\\',d,e,f']" ],
-                [ "a", "#id", "input[some-attr]", ".class", "input[some-attr='\\'']" ]
-            ]
-        }
-    },
+                'Single quotes': [
+                    [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = '' ]" ],
+                    [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c' ]" ],
+                    [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\],d,e,f' ]" ],
+                    [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\',d,e,f' ]" ],
+                    [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
+                    [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
+                    [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = '\\'' ]" ]
+                ]
+            },
 
-    'With spaces': {
-        'Without tag names': {
-            'Double quotes': [
-                [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "" ]' ],
-                [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c" ]' ],
-                [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\],d,e,f" ]' ],
-                [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\",d,e,f" ]' ],
-                [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
-                [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
-                [ 'a', '#id', '[ some-attr ]', '.class', '[ some-attr = "\\"" ]' ]
-            ],
+            'With tag names': {
+                'Double quotes': [
+                    [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "" ]' ],
+                    [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c" ]' ],
+                    [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\],d,e,f" ]' ],
+                    [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\",d,e,f" ]' ],
+                    [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
+                    [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
+                    [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "\\"" ]' ]
+                ],
 
-            'Single quotes': [
-                [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = '' ]" ],
-                [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c' ]" ],
-                [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\],d,e,f' ]" ],
-                [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\',d,e,f' ]" ],
-                [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
-                [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
-                [ "a", "#id", "[ some-attr ]", ".class", "[ some-attr = '\\'' ]" ]
-            ]
+                'Single quotes': [
+                    [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = '' ]" ],
+                    [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c' ]" ],
+                    [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\],d,e,f' ]" ],
+                    [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\',d,e,f' ]" ],
+                    [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
+                    [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
+                    [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = '\\'' ]" ]
+                ]
+            }
         },
 
-        'With tag names': {
-            'Double quotes': [
-                [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "" ]' ],
-                [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c" ]' ],
-                [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\],d,e,f" ]' ],
-                [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\",d,e,f" ]' ],
-                [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
-                [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "a,b,c\\]\\",d,e,f" ]' ],
-                [ 'a', '#id', 'input[ some-attr ]', '.class', 'input[ some-attr = "\\"" ]' ]
-            ],
+        'Other': [
+            [ '#TestDiv p' ],
+            [ "a", "#id", "[some-attr]", ".class", "[asdf\\]]" ],
+            [ '#a > div', ':first', '#id:first', '.class:first', '[attr]', '#id[attr]', '.class[attr]', '.class[attr]:first', '[attr="value"]', ':not([attr="value"])' ]
+        ],
 
-            'Single quotes': [
-                [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = '' ]" ],
-                [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c' ]" ],
-                [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\],d,e,f' ]" ],
-                [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\',d,e,f' ]" ],
-                [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
-                [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = 'a,b,c\\]\\',d,e,f' ]" ],
-                [ "a", "#id", "input[ some-attr ]", ".class", "input[ some-attr = '\\'' ]" ]
-            ]
+        'Pseudo': [
+            [ ":not([disabled])" ],
+            [ "input:not([disabled])" ],
+            [ "a :not([disabled])" ],
+            [ "a input:not([disabled])" ],
+            [ "a", "input:not([disabled])" ],
+            [ ':not([disabled="\\]"])' ],
+            [ 'input:not([disabled="\\]"])' ],
+            [ 'a :not([disabled="\\]"])' ],
+            [ 'a input:not([disabled="\\]"])' ],
+            [ 'a', 'input:not([disabled="\\]"])' ],
+            [ ':not([disabled="()"])' ],
+            [ 'input:not([disabled="()"])' ],
+            [ 'a :not([disabled="()"])' ],
+            [ 'a input:not([disabled="()"])' ],
+            [ 'a', 'input:not([disabled="()"])' ]
+        ]
+    };
+
+    var parser = D.__INTERNAL.fizzle.selector.parser;
+
+    var testQuery = function(expectedArray) {
+        var expected = expectedArray.join(', '),
+            actual   = parser.subqueries(expected).join(', ');
+
+        strictEqual(expected, actual);
+    };
+
+    var testQueries = function(queries, name) {
+        var len = queries.length,
+            idx = 0;
+
+        test(name, function() {
+            expect(len);
+
+            for (; idx < len; idx++) {
+                testQuery(queries[idx]);
+            }
+        });
+    };
+
+    var walk = function(node, name) {
+        if (_.isArray(node)) {
+            testQueries(node, name);
+        } else {
+            for (var prop in node) {
+                var newName = name ? (name + ': ' + prop) : prop;
+                walk(node[prop], newName);
+            }
         }
-    },
+    };
 
-    'Other': [
-        [ '#TestDiv p' ],
-        [ "a", "#id", "[some-attr]", ".class", "[asdf\\]]" ],
-        [ '#a > div', ':first', '#id:first', '.class:first', '[attr]', '#id[attr]', '.class[attr]', '.class[attr]:first', '[attr="value"]', ':not([attr="value"])' ]
-    ],
+    walk(QUERIES, 'Selector parser');
 
-    'Pseudo': [
-        [ ":not([disabled])" ],
-        [ "input:not([disabled])" ],
-        [ "a :not([disabled])" ],
-        [ "a input:not([disabled])" ],
-        [ "a", "input:not([disabled])" ],
-        [ ':not([disabled="\\]"])' ],
-        [ 'input:not([disabled="\\]"])' ],
-        [ 'a :not([disabled="\\]"])' ],
-        [ 'a input:not([disabled="\\]"])' ],
-        [ 'a', 'input:not([disabled="\\]"])' ],
-        [ ':not([disabled="()"])' ],
-        [ 'input:not([disabled="()"])' ],
-        [ 'a :not([disabled="()"])' ],
-        [ 'a input:not([disabled="()"])' ],
-        [ 'a', 'input:not([disabled="()"])' ]
-    ]
-};
-
-var parser = D.__INTERNAL.fizzle.selector.parser;
-
-var testQuery = function(expectedArray) {
-    var expected = expectedArray.join(', '),
-        actual   = parser.subqueries(expected).join(', ');
-
-    strictEqual(expected, actual);
-};
-
-var testQueries = function(queries, name) {
-    var len = queries.length,
-        idx = 0;
-
-    test(name, function() {
-        expect(len);
-
-        for (; idx < len; idx++) {
-            testQuery(queries[idx]);
-        }
-    });
-};
-
-var walk = function(node, name) {
-    if (_.isArray(node)) {
-        testQueries(node, name);
-    } else {
-        for (var prop in node) {
-            var newName = name ? (name + ': ' + prop) : prop;
-            walk(node[prop], newName);
-        }
-    }
-};
-
-walk(QUERIES, 'Selector parser');
+}());
