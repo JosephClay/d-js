72f9881f6020152cb387f164467c6b4ea0b7f119
Joseph Clay <clay@dcim.com>
2014-05-14 11:31:07 -0500

    Double bang to ensure a boolean

--- src/utils.js
+++ src/utils.js
@@ -4,7 +4,7 @@ var _ = require('_'),
 
 module.exports = {
     isAttached: function(elem) {
-        return (elem && elem !== document ? elem.parentNode : false);
+        return !!(elem && elem !== document ? elem.parentNode : false);
     },
 
     isHtml: function(text) {
