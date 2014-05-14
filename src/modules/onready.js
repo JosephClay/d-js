d14e3a84659213c48d05d3884b1f31813fe3b427
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-14 15:10:31 -0500

    Minor/ignore - Added comments

--- src/modules/onready.js
+++ src/modules/onready.js
@@ -2,17 +2,24 @@ var _isReady = false,
     _registration = [];
 
 var _bind = function(fn) {
+    // Already loaded
     if (document.readyState === 'complete') {
         return fn();
     }
 
+    // Standards-based browsers support DOMContentLoaded
     if (document.addEventListener) {
         return document.addEventListener('DOMContentLoaded', fn);
     }
 
+    // If IE event model is used
+
+    // Ensure firing before onload, maybe late but safe also for iframes
     document.attachEvent('onreadystatechange', function() {
         if (document.readyState === 'interactive') { fn(); }
     });
+
+    // A fallback to window.onload, that will always work
     window.attachEvent('onload', fn);
 };
 
