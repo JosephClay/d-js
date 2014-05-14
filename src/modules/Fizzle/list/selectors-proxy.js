668c093b0f315197f4dc61c4930d5785914ac8d5
Joseph Clay <clay@dcim.com>
2014-05-14 17:05:44 -0500

    Add an :input proxy

--- src/modules/Fizzle/list/selectors-proxy.js
+++ src/modules/Fizzle/list/selectors-proxy.js
@@ -11,6 +11,7 @@ module.exports = {
     ':reset'   : '[type="reset"]',
     ':button'  : '[type="button"]',
     ':image'   : '[type="image"]',
+    ':input'   : '[type="input"]',
     ':file'    : '[type="file"]',
     ':enabled' : ':not([disabled])',
     ':disabled': '[disabled]',
