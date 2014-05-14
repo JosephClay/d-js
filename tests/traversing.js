1a38a35b85debc9a8f942d39ede9d04cf72d3594
Joseph Clay <clay@dcim.com>
2014-05-14 11:22:34 -0500

    Minor/Ignore - formatting

--- tests/traversing.js
+++ tests/traversing.js
@@ -111,7 +111,6 @@ test('index()', function() {
     expect(2);
 
     equal(D('#text2').index(), 2, 'Returns the index of a child amongst its siblings');
-
     equal(D('<div/>').index(), -1, 'Node without parent returns -1');
 });
 
