46bf81fe146c0078f30374753799c2552d42b7db
Joseph Clay <testingriven@gmail.com>
2014-05-16 09:59:54 -0500

    Selector enhancements for finding ids within elements

--- src/modules/Fizzle/list/selectors-capture.js
+++ src/modules/Fizzle/list/selectors-capture.js
@@ -1,5 +1,5 @@
 module.exports = {
-	':eq': ':nth-child(x)',
-	':gt': ':nth-child(n+x)',
-	':lt': ':nth-child(~n+x)'
+	':eq':  ':nth-child(x)',
+	':gt':  ':nth-child(n+x)',
+	':lt':  ':nth-child(~n+x)'
 };
