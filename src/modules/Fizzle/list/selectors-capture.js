0493a75bd92fca41a0bcd10c4e2942bf1a8708fc
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-27 15:11:40 -0500

    Minor - Added TODO

--- src/modules/Fizzle/list/selectors-capture.js
+++ src/modules/Fizzle/list/selectors-capture.js
@@ -1,4 +1,5 @@
 module.exports = {
+    // TODO: Document differences between jQuery and D.js behavior for these selectors
 	':eq':  ':nth-child(x)',
 	':gt':  ':nth-child(n+x)',
 	':lt':  ':nth-child(~n+x)'
