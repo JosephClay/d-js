1075a93c4b3d9cca24b3ade37d387e9ef0d1dcf3
Andrew C. Dvorak <acdvorak@gmail.com>
2014-05-29 14:10:13 -0500

    Fixed sort ordering for disconnected nodes

--- src/order.js
+++ src/order.js
@@ -1,37 +1,44 @@
 var _NODE_TYPE = require('./nodeType'),
-    _DOC_POS   = require('./docPos')
-;
+    _DOC_POS   = require('./docPos'),
+    _supports  = require('./supports'),
+    _utils     = require('./utils');
 
 // Compare Position - MIT Licensed, John Resig
+// TODO: Optimize this function
 var _comparePosition = function(node1, node2) {
     // Modern browsers (IE9+)
     if (node1.compareDocumentPosition) {
         return node1.compareDocumentPosition(node2);
     }
 
+    var rel = 0;
+
+    if (node1 === node2) {
+        return rel;
+    }
+
     // IE8
     if (node1.contains) {
-        var rel = 0;
-
-        if (node1 !== node2 && node1.contains(node2)) {
+        if (node1.contains(node2)) {
             rel += _DOC_POS.CONTAINED_BY;
         }
-
-        if (node1 !== node2 && node2.contains(node1)) {
+        if (node2.contains(node1)) {
             rel += _DOC_POS.CONTAINS;
         }
 
         if (node1.sourceIndex >= 0 && node2.sourceIndex >= 0) {
             rel += (node1.sourceIndex < node2.sourceIndex ? _DOC_POS.FOLLOWING : 0);
             rel += (node1.sourceIndex > node2.sourceIndex ? _DOC_POS.PRECEDING : 0);
+
+            if (!_utils.isAttached(node1) || !_utils.isAttached(node2)) {
+                rel += _DOC_POS.DISCONNECTED;
+            }
         } else {
             rel += _DOC_POS.DISCONNECTED;
         }
-
-        return rel;
     }
 
-    return 0;
+    return rel;
 };
 
 var _is = function(rel, flag) {
@@ -43,7 +50,7 @@ var _isNode = function(b, flag, a) {
     return _is(rel, flag);
 };
 
-module.exports = {
+var _order = module.exports = {
 
     /**
      * Sorts an array of DOM elements in-place (i.e., mutates the original array)
@@ -63,18 +70,38 @@ module.exports = {
                     return 0;
                 }
 
-                var rel = _comparePosition(a, b);
+                // Sort on method existence if only one input has compareDocumentPosition
+                var rel = !a.compareDocumentPosition - !b.compareDocumentPosition;
+                if (rel) {
+                    return rel;
+                }
 
-                // Not directly comparable, sort on existence of method
-                if (!rel) { return a.compareDocumentPosition ? -1 : 1; }
+                // Nodes share the same document
+                if ((a.ownerDocument || a) === (b.ownerDocument || b)) {
+                    rel = _comparePosition(a, b);
+                }
+                // Otherwise we know they are disconnected
+                else {
+                    rel = _DOC_POS.DISCONNECTED;
+                }
+
+                // Not directly comparable
+                if (!rel) {
+                    // Maintain original order
+                    return 0;
+                }
 
                 // Disconnected nodes
                 if (_is(rel, _DOC_POS.DISCONNECTED)) {
-                    // Choose the first element that is related to our document
-                    if (a === document || b === document) { return 1; }
+                    var isDisconnectedA = !_utils.isAttached(a);
+                    var isDisconnectedB = !_utils.isAttached(b);
 
-                    // Maintain original order
-                    return 0;
+                    if (isDisconnectedA && isDisconnectedB) {
+                        // Maintain original order
+                        return 0;
+                    }
+
+                    return isDisconnectedB ? -1 : 1;
                 }
 
                 return _is(rel, _DOC_POS.FOLLOWING) ? -1 : 1;
@@ -98,7 +125,7 @@ module.exports = {
      */
     contains: function(a, b) {
         var aDown = a.nodeType === _NODE_TYPE.DOCUMENT ? a.documentElement : a,
-            bUp   = b && b.parentNode;
+            bUp   = _utils.isAttached(b) ? b.parentNode : null;
 
         if (a === bUp) {
             return true;
