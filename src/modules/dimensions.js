de2ed0be3124d37e074d6be55a1b2ce31a4a7438
Andrew C. Dvorak <acdvorak@gmail.com>
2014-06-04 16:32:43 -0500

    Cleanup

--- src/modules/dimensions.js
+++ src/modules/dimensions.js
@@ -5,16 +5,14 @@ var _ = require('_'),
     _css = require('./css');
 
 var _getInnerWidth = function(elem) {
-        var width = parseFloat(_css.width.get(elem)) || 0,
-            style = _css.getComputedStyle(elem) || {};
+        var width = parseFloat(_css.width.get(elem)) || 0;
 
         return width +
             (_.parseInt(_css.curCss(elem, 'paddingLeft')) || 0) +
                 (_.parseInt(_css.curCss(elem, 'paddingRight')) || 0);
     },
     _getInnerHeight = function(elem) {
-        var height = parseFloat(_css.height.get(elem)) || 0,
-            style = _css.getComputedStyle(elem) || {};
+        var height = parseFloat(_css.height.get(elem)) || 0;
 
         return height +
             (_.parseInt(_css.curCss(elem, 'paddingTop')) || 0) +
@@ -22,8 +20,7 @@ var _getInnerWidth = function(elem) {
     },
 
     _getOuterWidth = function(elem, withMargin) {
-        var width = _getInnerWidth(elem),
-            style = _css.getComputedStyle(elem) || {};
+        var width = _getInnerWidth(elem);
 
         if (withMargin) {
             width += (_.parseInt(_css.curCss(elem, 'marginLeft')) || 0) +
@@ -35,8 +32,7 @@ var _getInnerWidth = function(elem) {
                 (_.parseInt(_css.curCss(elem, 'borderRightWidth')) || 0);
     },
     _getOuterHeight = function(elem, withMargin) {
-        var height = _getInnerHeight(elem),
-            style = _css.getComputedStyle(elem) || {};
+        var height = _getInnerHeight(elem);
 
         if (withMargin) {
             height += (_.parseInt(_css.curCss(elem, 'marginTop')) || 0) +
