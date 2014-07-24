e5f167f235648c8331792871c2c6847a44cf04ca
Andy Dvorak <dvorak@dcim.com>
2014-07-24 16:57:35 -0500

    Minor/ignore - Formatting

--- src/modules/classes.js
+++ src/modules/classes.js
@@ -7,7 +7,8 @@ var _         = require('_'),
     _split    = _.string.split,
     _isEmpty  = _.string.isEmpty;
 
-var _impl = _SUPPORTS.classList ? require('./classes/classes-modern') : require('./classes/classes-legacy');
+var _impl = _SUPPORTS.classList ? require('./classes/classes-modern')
+                                : require('./classes/classes-legacy');
 
 var _doAnyElemsHaveClass = function(elems, name) {
         var elemIdx = elems.length;
