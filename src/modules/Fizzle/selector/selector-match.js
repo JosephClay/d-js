cf680a6037bc819593b444f251aba1cd4030aecd
Andy Dvorak <dvorak@dcim.com>
2014-07-30 15:02:27 -0500

    Fixed failing unit test in IE8

--- src/modules/Fizzle/selector/selector-match.js
+++ src/modules/Fizzle/selector/selector-match.js
@@ -4,23 +4,41 @@ var _SUPPORTS  = require('../../../supports'),
     _matchesSelector = _SUPPORTS.matchesSelector;
 
 var matches;
+
+// IE9+, modern browsers
 if (_matchesSelector) {
     matches = function(elem, selector) {
         if (elem.nodeType !== _NODE_TYPE.ELEMENT) { return false; }
 
         return _matchesSelector.call(elem, selector);
     };
-} else {
+}
+// IE8
+else {
     matches = function(elem, selector) {
         if (elem.nodeType !== _NODE_TYPE.ELEMENT) { return false; }
 
-        var nodes = elem.parentNode.querySelectorAll(selector),
+        var frag;
+
+        if (!elem.parentNode) {
+            frag = document.createDocumentFragment();
+            frag.appendChild(elem);
+        }
+
+        var nodes = (frag || elem.parentNode).querySelectorAll(selector),
             idx = nodes.length;
+
         while (idx--) {
             if (nodes[idx] === elem) {
                 return true;
             }
         }
+
+        if (frag) {
+            frag.removeChild(elem);
+            frag = null; // prevent memory leaks in IE8
+        }
+
         return false;
     };
 }
