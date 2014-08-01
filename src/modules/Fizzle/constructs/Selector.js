130b1c45e6150286e5d192d8c30c8f644fa15431
Joseph Clay <clay@dcim.com>
2014-08-01 15:20:10 -0500

    selector refactor for readability and performance

--- src/modules/Fizzle/constructs/Selector.js
+++ src/modules/Fizzle/constructs/Selector.js
@@ -1,5 +1,8 @@
 var _ = require('_'),
 
+    overload = require('overload'),
+    O = overload.O,
+
     _ID_PREFIX = 'D-uniqueId-',
 
     _GET_ELEMENT_BY_ID          = 'getElementById',
@@ -34,9 +37,11 @@ var _determineMethod = function(selector) {
         _querySelectorCache.set(selector, method);
         return method;
     },
+
     _uniqueId = function() {
         return _ID_PREFIX + _.uniqueId();
     },
+
     _fromDomArrayToArray = function(arrayLike) {
         var idx = arrayLike.length,
             arr = new Array(idx);
@@ -44,6 +49,95 @@ var _determineMethod = function(selector) {
             arr[idx] = arrayLike[idx];
         }
         return arr;
+    },
+
+    _processQuerySelection = function(selection) {
+        // No selection
+        if (!selection) {
+            return [];
+        }
+        // Nodelist without a length
+        if (_.isNodeList(selection) && !selection.length) {
+            return [];
+        }
+        // IE8 DispStaticNodeList
+        if (selection.item && selection.length === 0) {
+            return [];
+        }
+
+        // If it's an id, return it as an array
+        return _.isElement(selection) || !selection.length ? [selection] : _fromDomArrayToArray(selection);
+    },
+
+    _childOrSiblingQuery = function(context, self) {
+        // Child select - needs special help so that "> div" doesn't break
+        var method    = self.method,
+            idApplied = false,
+            selector  = self.selector,
+            newId,
+            id;
+
+        id = context.id;
+        if (id === '' || !_.exists(id)) {
+            newId = _uniqueId();
+            context.id = newId;
+            idApplied = true;
+        }
+
+        selector = self._tailorChildSelector(idApplied ? newId : id, selector);
+
+        var selection = document[method](selector);
+
+        if (idApplied) {
+            context.id = id;
+        }
+
+        return _processQuerySelection(selection);
+    },
+
+    _classQuery = function(context, self) {
+        var method   = self.method,
+            selector = self.selector;
+
+        if (_SUPPORTS.getElementsByClassName) {
+            // Class search, don't start with '.'
+            selector = selector.substr(1);
+        } else {
+            method = _QUERY_SELECTOR_ALL;
+        }
+
+        var selection = context[method](selector);
+
+        return _processQuerySelection(selection);
+    },
+
+    _idQuery = function(context, self) {
+        var method   = self.method,
+            selector = self.selector.substr(1),
+            selection = document[method](selector);
+
+        return _processQuerySelection(selection);
+    },
+
+    _defaultQuery = function(context, self) {
+        var selection = context[self.method](self.selector);
+        return _processQuerySelection(selection);
+    },
+
+    _determineQuery = function(self) {
+        if (self.isChildOrSiblingSelect) {
+            return _childOrSiblingQuery;
+        }
+
+        if (self.isClassSearch) {
+            return _classQuery;
+        }
+
+        if (self.isIdSearch) {
+            return _idQuery;
+        }
+
+        return _defaultQuery;
     };
 
 var Selector = function(str) {
@@ -74,69 +168,15 @@ Selector.prototype = {
     },
 
     exec: function(context) {
-        context = context || document;
+        var query = _determineQuery(this);
 
-        var selection,
-            method = this.method,
-            nodeType;
-
-        // Early return if context is not an element or document
-        if ((nodeType = context.nodeType) !== _NODE_TYPE.ELEMENT && nodeType !== _NODE_TYPE.DOCUMENT) { return; }
-
-        // Child select - needs special help so that "> div" doesn't break
-        var id,
-            newId,
-            idApplied = false,
-            selector  = this.selector;
-
-        if (this.isChildOrSiblingSelect) {
-            id = context.id;
-            if (id === '' || !_.exists(id)) {
-                newId = _uniqueId();
-                context.id = newId;
-                idApplied = true;
-            }
-
-            selector = this._tailorChildSelector(idApplied ? newId : id, selector);
-            context = document;
-        } else if (this.isClassSearch) {
-            if (_SUPPORTS.getElementsByClassName) {
-                // Class search, don't start with '.'
-                selector = selector.substr(1);
-            } else {
-                method = _QUERY_SELECTOR_ALL;
-            }
-        } else if (this.isIdSearch) {
-            if (context === document) {
-                // Id search, don't start with '#'
-                selector = selector.substr(1);
-            } else {
-                // context is not the document,
-                // change to querySelectorAll and
-                // leave the '#'
-                method = _QUERY_SELECTOR_ALL;
-            }
+        if (!context || context === document) {
+            return query(document, this);
         }
 
-        selection = context[method](selector);
-
-        if (idApplied) { context.id = id; }
-
-        // No selection
-        if (!selection) {
-            return [];
-        }
-        // Nodelist without a length
-        if (_.isNodeList(selection) && !selection.length) {
-            return [];
-        }
-        // IE8 DispStaticNodeList
-        if (selection.item && selection.length === 0) {
-            return [];
-        }
-
-        // If it's an id, return it as an array
-        return _.isElement(selection) || !selection.length ? [selection] : _fromDomArrayToArray(selection);
+        // these are the types we're expecting to fall through
+        // _.isElement(context) || _.isNodeList(context) || _.isCollection(context)
+        return query(context, this);
     }
 };
 
