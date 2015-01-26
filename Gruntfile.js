3a87f86bb785d5ebf3ca5b5f6e93886a8b1f270d
JosephClay <j053phclay@gmail.com>
2015-01-26 10:30:58 -0600

    remove commented out code

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,75 +1,11 @@
 module.exports = function(grunt) {
 
-    /*
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-rename');
-
-    var srcFiles = [
-        'build/intro.js',
-
-        'src/div.js',
-        'src/nodeType.js',
-
-        'src/utils.js',
-        'src/cache.js',
-        'src/supports.js',
-
-        'src/D.js',
-
-        'build/outro.js'
-    ];
-
-    grunt.initConfig({
-        pkg: grunt.file.readJSON('package.json'),
-        concat: {
-            options: {
-                stripBanners: true,
-                separator: '\n\n//----\n\n',
-            },
-            dist: {
-                files: {
-                    'D.js': srcFiles
-                },
-                nonull: true
-            }
-        },
-        rename: {
-            basic: {
-                files: [
-                    {
-                        src: 'D.js',
-                        dest: 'out/D.js'
-                    }
-                ]
-            }
-        }
-    });
-
-    grunt.registerTask('default', [
-        'concat',
-        'rename:basic'
-    ]);
-    */
-
     grunt.loadNpmTasks('grunt-browserify');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-rquirejs');
 
     grunt.initConfig({
-        browserify: {
-            build: {
-                src: ['src/D.js'],
-                dest: 'dist/D.browserify.js'
-                // options: {
-                //     alias: [
-                //         'src/libs/Overload.js:overload',
-                //         'src/libs/Signal.js:signal',
-                //         'src/libs/underscore.js:underscore'
-                //     ]
-                // }
-            }
-        },
         watch: {
             scripts: {
                 files: ['src/**/*.js'],
@@ -110,16 +46,9 @@ module.exports = function(grunt) {
                     micro_paths: false,
                 }
             }
-        },
-        uglify: {
-            release: {
-                files: {
-                    'dist/D.min.js': ['dist/D.js']
-                }
-            }
         }
     });
 
-    grunt.registerTask('build', ['rquire', 'uglify']);
+    grunt.registerTask('build', ['rquire']);
     grunt.registerTask('default', ['build', 'watch']);
 };
