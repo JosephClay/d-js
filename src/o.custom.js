7ff788f4db562763a3c0f7709bcd20009bc85d41
JosephClay <j053phclay@gmail.com>
2015-01-26 12:39:29 -0600

    formatting

--- src/o.custom.js
+++ src/o.custom.js
@@ -1,4 +1,4 @@
-var _ = require('underscore'),
+var _        = require('underscore'),
     overload = require('overload-js');
 
 // Configure overload to throw type errors
@@ -6,17 +6,17 @@ overload.err = function() {
     throw new TypeError();
 };
 
-var _isD = function(val) {
+var isD = function(val) {
     return val instanceof D;
 };
 
-var _isCollection = function(val) {
-    return _isD(val) || _.isArray(val) || _.isNodeList(val);
+var isCollection = function(val) {
+    return isD(val) || _.isArray(val) || _.isNodeList(val);
 };
 
 overload.defineTypes({
     'D': function(val) {
-        return val && _isD(val);
+        return val && isD(val);
     },
     'nodeList': function(val) {
         return _.isNodeList(val);
@@ -28,10 +28,10 @@ overload.defineTypes({
         return val && val === document;
     },
     'selector': function(val) {
-        return val && (_.isString(val) || _.isFunction(val) || _.isElement(val) || _isCollection(val));
+        return val && (_.isString(val) || _.isFunction(val) || _.isElement(val) || isCollection(val));
     },
     'collection': function(val) {
-        return val && _isCollection(val);
+        return val && isCollection(val);
     },
     'element': function(val) {
         return val === document || _.isWindow(val) || _.isElement(val);
