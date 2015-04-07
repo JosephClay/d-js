b1fc0257bc77fa370b458bbee989c8d1ebc5ec95
JosephClay <j053phclay@gmail.com>
2015-04-07 09:50:27 -0500

    cache updates

--- src/cache/index.js
+++ src/cache/index.js
@@ -1,46 +1,50 @@
+var deleter = function(deletable) {
+    return deletable ? 
+        function(store, key) { delete store[key]; } :
+        function(store, key) { store[key] = undefined; };
+};
+
 // TODO: Deprecate getOrSet
 var getterSetter = function(deletable) {
-    var store = {};
+    var store = {},
+        del = deleter(deletable);
 
     return {
         has: function(key) {
-            return key in store;
+            return key in store && store[key] !== undefined;
         },
         get: function(key) {
-            return store[key];
+            return this.has(key) ? store[key] : undefined;
         },
         set: function(key, value) {
             store[key] = value;
             return value;
         },
         getOrSet: function(key, fn) {
-            var cachedVal = store[key];
-            if (cachedVal !== undefined) { return cachedVal; }
+            var value = this.get(key);
+            if (value !== undefined) { return value; }
             return (store[key] = fn());
         },
         remove: function(key) {
-            if (deletable) {
-                delete store[key];
-            } else {
-                store[key] = undefined;
-            }
+            del(store, key);
         }
     };
 };
 
 var biLevelGetterSetter = function(deletable) {
-    var store = {};
+    var store = {},
+        del = deleter(deletable);
 
     return {
         has: function(key1) {
-            return store[key1] !== undefined;
+            return key1 in store && store[key1] !== undefined;
         },
         get: function(key1, key2) {
-            var ref1 = store[key1];
+            var ref1 = this.has(key1) ? store[key1] : undefined;
             return arguments.length === 1 ? ref1 : (ref1 !== undefined ? ref1[key2] : ref1);
         },
         set: function(key1, key2, value) {
-            var ref1 = store[key1] || (store[key1] = {});
+            var ref1 = this.has(key1) ? store[key1] : (store[key1] = {});
             ref1[key2] = value;
             return value;
         },
@@ -53,21 +57,12 @@ var biLevelGetterSetter = function(deletable) {
         remove: function(key1, key2) {
             // Easy removal
             if (arguments.length === 1) {
-                if (deletable) {
-                    delete store[key1];
-                } else {
-                    store[key1] = undefined;
-                }
-                return;
+                return del(store, key1);
             }
 
             // Deep removal
             var ref1 = store[key1] || (store[key1] = {});
-            if (deletable) {
-                delete ref1[key2];
-            } else {
-                ref1[key2] = undefined;
-            }
+            del(ref1, key2);
         }
     };
 };
